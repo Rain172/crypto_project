7680187d1bf4fcaeb9f0c237bf93022814b7816d
Rain172 <144408130+Rain172@users.noreply.github.com>
2023-09-18 09:57:56 +0700

    Final Changes

--- cyrto.py
+++ cyrto.py
@@ -1,158 +1,163 @@
-import socket
-import pickle
-import threading
-import time
 import hashlib
-import copy
-
-DIFFICULTY = 4
+import time
+import json
 
 class Block:
-    def __init__(self, index, previous_hash, timestamp, data, hash, nonce):
-        self.index = index
-        self.previous_hash = previous_hash
-        self.timestamp = timestamp
-        self.data = data
-        self.hash = hash
-        self.nonce = nonce
+    def __init__(self, index, previous_hash, timestamp, data, nonce=0, hash=None):
+        self.index = index  # Block's position in the chain
+        self.previous_hash = previous_hash  # Hash of the previous block
+        self.timestamp = timestamp  # Timestamp of when the block was created
+        self.data = data  # Data stored in the block
+        self.nonce = nonce  # Nonce used for Proof of Work
+        self.hash = self.calculate_hash() if hash is None else hash  # Block's hash
 
     def calculate_hash(self):
-        value = str(self.index) + str(self.previous_hash) + str(self.timestamp) + str(self.data) + str(self.nonce)
+        # Calculate the hash of the block based on its attributes
+        value = str(self.index) + self.previous_hash + str(self.timestamp) + str(self.data) + str(self.nonce)
         return hashlib.sha256(value.encode()).hexdigest()
-
-def proof_of_work(index, previous_hash, timestamp, data):
-    nonce = 0
-    target = "0" * DIFFICULTY
-    while True:
-        value = str(index) + str(previous_hash) + str(timestamp) + str(data) + str(nonce)
-        hash = hashlib.sha256(value.encode()).hexdigest()
-        if hash[:DIFFICULTY] == target:
-            return nonce
-        nonce += 1
 
 class Blockchain:
     def __init__(self):
-        self.chain = [self.create_genesis_block()]
-        self.pending_transactions = []
-        self.lock = threading.Lock()  # Lock for thread safety
+        # Initialize the blockchain with a list containing the genesis block
+        self.chain = self.load_chain_from_file() or [self.create_genesis_block()]
+        self.pending_transactions = []  # Transactions waiting to be mined
+        self.wallets = self.load_wallets_from_file() or {"Wallet1": 100, "Wallet2": 100, "Wallet3": 100, "Wallet4": 100}
+        self.mining_reward = 10  # Reward for mining a block
+        self.difficulty = 4  # Difficulty of PoW
+
+    def save_chain_to_file(self):
+        # Save the blockchain to a JSON file
+        with open('blockchain.json', 'w') as file:
+            json.dump([block.__dict__ for block in self.chain], file)
+
+    def load_chain_from_file(self):
+        # Load the blockchain from a JSON file, if it exists
+        try:
+            with open('blockchain.json', 'r') as file:
+                data = json.load(file)
+                return [Block(**block) for block in data]
+        except FileNotFoundError:
+            return None
+
+    def save_wallets_to_file(self):
+        # Save wallet balances to a JSON file
+        with open('wallets.json', 'w') as file:
+            json.dump(self.wallets, file)
+
+    def load_wallets_from_file(self):
+        # Load wallet balances from a JSON file, if it exists
+        try:
+            with open('wallets.json', 'r') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            return None
 
     def create_genesis_block(self):
-        return Block(0, "0", int(time.time()), "Genesis Block", self.calculate_hash(0, "0", int(time.time()), "Genesis Block", 0), 0)
+        # Create the first block (genesis block) of the blockchain
+        return Block(0, "0", int(time.time()), "Genesis Block")
 
-    def add_block(self, block):
-        self.chain.append(block)
-
-    def get_previous_block(self):
+    def get_last_block(self):
+        # Get the last block in the blockchain
         return self.chain[-1]
 
-    def create_new_block(self, nonce):
-        previous_block = self.get_previous_block()
-        index = previous_block.index + 1
-        timestamp = int(time.time())
-        transactions = list(self.pending_transactions)
-        self.pending_transactions = []  # Clear pending transactions
-        hash = self.calculate_hash(index, previous_block.hash, timestamp, transactions, nonce)
-        return Block(index, previous_block.hash, timestamp, transactions, hash, nonce)
+    def add_block(self, new_block):
+        # Add a new block to the blockchain if it's valid
+        if self.is_valid_block(new_block):
+            self.chain.append(new_block)
+            print("Block #{} has been added to the blockchain!".format(new_block.index))
+            return True
+        return False
 
-    def validate_blockchain(self):
-        for i in range(1, len(self.chain)):
-            current_block = self.chain[i]
-            previous_block = self.chain[i - 1]
-            if (
-                current_block.hash
-                != self.calculate_hash(
-                    current_block.index,
-                    previous_block.hash,
-                    current_block.timestamp,
-                    current_block.data,
-                    current_block.nonce,
-                )
-                or current_block.hash[:DIFFICULTY] != "0" * DIFFICULTY
-            ):
-                return False
+    def is_valid_block(self, block):
+        # Check the validity of a block
+        if block.index != len(self.chain):
+            return False
+        if block.previous_hash != self.get_last_block().hash:
+            return False
+        if not self.is_valid_proof(block):
+            return False
         return True
 
-    def add_transaction(self, transaction):
+    def is_valid_proof(self, block):
+        # Check if the Proof of Work for a block is valid
+        guess = str(block.index) + block.previous_hash + str(block.timestamp) + str(block.data) + str(block.nonce)
+        guess_hash = hashlib.sha256(guess.encode()).hexdigest()
+        return guess_hash[:self.difficulty] == "0" * self.difficulty
+
+    def mine_block(self, miner_address):
+        # Mine a new block and add it to the blockchain
+        reward = self.mining_reward  # Reward for mining a block
+        transactions = self.pending_transactions[:]
+        transactions.append({"from": "Network", "to": miner_address, "amount": reward})
+
+        new_block = Block(len(self.chain), self.get_last_block().hash, int(time.time()), transactions)
+        while not self.is_valid_proof(new_block):
+            new_block.nonce += 1
+
+        self.add_block(new_block)
+        self.pending_transactions = []
+
+        # Credit mining reward to miner's wallet
+        self.wallets[miner_address] += reward
+
+    def create_transaction(self, sender, recipient, amount):
+        # Create a new transaction and update wallet balances
+        if sender not in self.wallets or recipient not in self.wallets:
+            print("Invalid sender or recipient.")
+            return
+
+        if self.wallets[sender] < amount:
+            print("Insufficient balance in sender's wallet.")
+            return
+
+        transaction = {"from": sender, "to": recipient, "amount": amount}
         self.pending_transactions.append(transaction)
+        self.wallets[sender] -= amount
+        self.wallets[recipient] += amount
+        print("Transaction created.")
 
-    def calculate_hash(self, index, previous_hash, timestamp, data, nonce):
-        value = str(index) + str(previous_hash) + str(timestamp) + str(data) + str(nonce)
-        return hashlib.sha256(value.encode()).hexdigest()
+# Initialize the blockchain
+blockchain = Blockchain()
 
-class Transaction:
-    def __init__(self, sender, recipient, amount):
-        self.sender = sender
-        self.recipient = recipient
-        self.amount = amount
+while True:
+    print("\nOptions:")
+    print("1: View Blockchain")
+    print("2: Create Transaction")
+    print("3: Mine Block")
+    print("4: View Wallet Balances")
+    print("5: Save Blockchain and Wallets")
+    print("6: Exit")
 
-class Node:
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind((self.host, self.port))
-        self.peers = []
-        self.blockchain = Blockchain()
+    choice = input("Enter your choice: ")
 
-    def listen_for_connections(self):
-        self.socket.listen(5)
-        print(f"Listening for connections on {self.host}:{self.port}")
-        while True:
-            client_socket, _ = self.socket.accept()
-            peer = client_socket.getpeername()
-            print(f"Accepted connection from {peer}")
-            self.peers.append(client_socket)
-            threading.Thread(target=self.handle_client, args=(client_socket,)).start()
-
-    def handle_client(self, client_socket):
-        while True:
-            try:
-                data = client_socket.recv(1024)
-                if data:
-                    message = pickle.loads(data)
-                    if message.get("type") == "block":
-                        self.handle_received_block(message["block"])
-            except Exception as e:
-                self.peers.remove(client_socket)
-                print(f"Connection closed by peer {client_socket.getpeername()}, Error: {e}")
-                break
-
-    def handle_received_block(self, received_block):
-        with self.blockchain.lock:  # Ensure thread safety
-            if received_block.index > self.blockchain.get_previous_block().index:
-                if self.blockchain.validate_blockchain():
-                    self.blockchain.add_block(received_block)
-                    self.broadcast_block(received_block)
-                    print(f"Received and added new block #{received_block.index}")
-
-    def broadcast_block(self, block):
-        for peer_socket in self.peers:
-            try:
-                peer_socket.sendall(pickle.dumps({"type": "block", "block": block}))
-            except Exception as e:
-                self.peers.remove(peer_socket)
-                print(f"Connection with peer {peer_socket.getpeername()} lost, Error: {e}")
-
-def main():
-    node1 = Node("10.0.97.119", 5000)  # Update with the appropriate IP address and port
-    node2 = Node("10.0.97.119", 5001)  # Update with the appropriate IP address and port
-
-    threading.Thread(target=node1.listen_for_connections).start()
-    threading.Thread(target=node2.listen_for_connections).start()
-
-    transaction1 = Transaction("Alice", "Bob", 10)
-    node1.blockchain.add_transaction(transaction1)
-
-    transaction2 = Transaction("Bob", "Charlie", 5)
-    node2.blockchain.add_transaction(transaction2)
-
-    new_block1 = node1.blockchain.create_new_block(proof_of_work(node1.blockchain.get_previous_block().index + 1, node1.blockchain.get_previous_block().hash, int(time.time()), node1.blockchain.pending_transactions))
-    node1.blockchain.add_block(new_block1)
-    node1.broadcast_block(new_block1)
-
-    new_block2 = node2.blockchain.create_new_block(proof_of_work(node2.blockchain.get_previous_block().index + 1, node2.blockchain.get_previous_block().hash, int(time.time()), node2.blockchain.pending_transactions))
-    node2.blockchain.add_block(new_block2)
-    node2.broadcast_block(new_block2)
-
-if __name__ == "__main__":
-    main()
+    if choice == "1":
+        # View the blockchain
+        for block in blockchain.chain:
+            print("\nBlock #{}".format(block.index))
+            print("Timestamp: {}".format(block.timestamp))
+            print("Data: {}".format(block.data))
+            print("Hash: {}".format(block.hash))
+    elif choice == "2":
+        # Create a transaction
+        sender = input("Enter sender's address: ")
+        recipient = input("Enter recipient's address: ")
+        amount = int(input("Enter amount to send: "))
+        blockchain.create_transaction(sender, recipient, amount)
+    elif choice == "3":
+        # Mine a block
+        miner_address = input("Enter miner's address: ")
+        blockchain.mine_block(miner_address)
+    elif choice == "4":
+        # View wallet balances
+        for wallet, balance in blockchain.wallets.items():
+            print(f"{wallet}: {balance}")
+    elif choice == "5":
+        # Save the blockchain and wallets to files
+        blockchain.save_chain_to_file()
+        blockchain.save_wallets_to_file()
+        print("Blockchain and Wallets saved.")
+    elif choice == "6":
+        # Exit the program
+        break
+    else:
+        print("Invalid choice. Please try again.")
